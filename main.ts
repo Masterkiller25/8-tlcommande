86f3284c1caceeb1d31943c2bdfef2a2cc18f0d4
Masterkiller25 <98768627+Masterkiller25@users.noreply.github.com>
2023-05-06 14:46:45 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,6 @@ basic.showLeds(`
     `)
 radio.setGroup(5)
 basic.forever(function () {
-    radio.sendValue("droite", input.acceleration(Dimension.X))
-    radio.sendValue("guache", input.acceleration(Dimension.Y))
+    radio.sendValue("x", input.acceleration(Dimension.X))
+    radio.sendValue("y", input.acceleration(Dimension.Y))
 })
